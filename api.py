648e229ef500fab6e5baf45a26eb694639ee4364
matias-cavalcante <matias.cavalcante93@gmail.com>
2023-02-19 22:19:18 +0000

    FLask testing app built. Now try deploying

---
+++ api.py
@@ -0,0 +1,23 @@
+
+
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import scrapN1
+
+app = Flask(__name__)
+CORS(app)
+
+
+# @app.route('/n1', methods=['GET'])
+def hello():
+    n1Data = scrapN1.clickAndFetch()
+    n1JsonNow = jsonify(n1Data)
+    return n1JsonNow
+
+
+print(hello())
+
+"""
+if __name__ == '__main__':
+    app.run(debug=True)
+"""
--- scrapN1.py
+++ scrapN1.py
@@ -5,40 +5,77 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium import webdriver
 
-
 # Browsing options
 options = webdriver.ChromeOptions()
+options.add_argument('--headless')
 options.add_argument('--start-maximized')
 options.add_argument('--disable-extensions')
 
 
 url = 'https://www.n1.is/thjonusta/eldsneyti/daeluverd/'
 
-
-driver = webdriver.Chrome()
+#driver = webdriver.Chrome()
+driver = webdriver.Chrome(options=options)
 driver.get(url)
 
 
-regionsBtns = '.css-1ssumb9.eoreqqy2'
-boxes = '.css-pdy5sh.epw6mxe0'
+def stationLocationName(data):
+    unfilteredName = data.find_element(By.TAG_NAME, "h4")
+    toFilterName = unfilteredName.text.split(" ")
+    if len(toFilterName) == 1:
+        return toFilterName[0]
+    elif len(toFilterName) == 2:
+        return toFilterName[1]
+    else:
+        return toFilterName[2]
 
 
-def clickAndFetch(regbuttons, cajas):
-    buttons = driver.find_elements(By.CSS_SELECTOR, regbuttons)
+def flawedPrice(price):
+    if price == '0':
+        return int(price)
+    elif len(price) == 0:
+        return "failed"
+
+
+def fuelTypeValueFinder(data, fuel):
+    fuelResults = data.find_element_by_xpath(
+        './/span[text()="{}"]'.format(fuel))
+    fuelPrice = fuelResults.find_element_by_xpath('following-sibling::div')
+    fuelPrice = fuelPrice.text
+    if flawedPrice(fuelPrice) == 0 or flawedPrice(fuelPrice):
+        return fuelPrice
+    fuelPriceConverted = float(fuelPrice.replace(",", "."))
+    return fuelPriceConverted
+
+
+def clickAndFetch():
+    jsonN1 = {}
+    bensin = '.css-1635yco.epw6mxe6'
+    buttons = driver.find_elements(By.CSS_SELECTOR, '.css-1ssumb9.eoreqqy2')
+    regionsSection = driver.find_elements(
+        By.CSS_SELECTOR, '.css-1nwxvlr.eoreqqy0')
+    regionsJumper = 0
 
     for btn in buttons:
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
-            (By.CSS_SELECTOR, regbuttons
-             )))
+        WebDriverWait(driver, 6).until(EC.element_to_be_clickable(
+            (By.CSS_SELECTOR, '.css-1ssumb9.eoreqqy2')))
         btn.click()
         time.sleep(2)
 
-        # Find the elements containing the data you want
-        data_elements = driver.find_elements(By.CSS_SELECTOR, cajas)
+        sectionData = regionsSection[regionsJumper].find_elements(
+            By.CSS_SELECTOR, '.css-pdy5sh.epw6mxe0')
 
-        # Extract the text from each element and print it
-        for elem in data_elements:
-            print(elem.text)
+        for block in sectionData:
+            stationName = stationLocationName(block)
+            bensinPrice = fuelTypeValueFinder(block, 'Bensín')
+            diselPrice = fuelTypeValueFinder(block, 'Dísel')
+            jsonN1[stationName] = {bensinPrice, diselPrice}
+        regionsJumper = regionsJumper + 1
+    return jsonN1
 
 
-clickAndFetch(regionsBtns, boxes)
+#n1PricesToData = clickAndFetch()
+
+
+# for key, value in n1PricesToData.items():
+#    print(f'{key}: {value}')
