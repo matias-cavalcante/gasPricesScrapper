cb6a75f003022a9eadd4de7aab281bf167249ec4
matias-cavalcante <matias.cavalcante93@gmail.com>
2023-02-19 22:27:29 +0000

    Api modified to allow request GET

--- api.py
+++ api.py
@@ -8,16 +8,12 @@ app = Flask(__name__)
 CORS(app)
 
 
-# @app.route('/n1', methods=['GET'])
+@app.route('/n1', methods=['GET'])
 def hello():
     n1Data = scrapN1.clickAndFetch()
     n1JsonNow = jsonify(n1Data)
     return n1JsonNow
 
 
-print(hello())
-
-"""
 if __name__ == '__main__':
     app.run(debug=True)
-"""
